78e541d824d8d1e4a3bd11b61491da6c0fcfd52f
Roland Strasser <rol1510@users.noreply.github.com>
2021-02-11 02:12:04 +0100

    don't ask for sudo password

--- my_aliases.sh
+++ my_aliases.sh
@@ -1,10 +1,12 @@
 EDITOR="code"
 
+alias startup="sudo sh ~/myconfig/startup.sh"
+
 alias ea="$EDITOR ~/myconfig/my_aliases.sh"
 alias c="$EDITOR ."
+
 alias fs="ls -lh"
 alias ds="df -h | grep sd"
-alias startup="sudo sh ~/myconfig/startup.sh"
 
 alias g="git"
 alias gs="git status --short"
