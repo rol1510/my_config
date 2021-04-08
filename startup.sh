dcd003f1a2ebb2375a10ea6a0367cf35532b9b1a
Roland Strasser <rol1510@users.noreply.github.com>
2021-04-08 14:22:32 +0200

    fix mount shares in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,5 @@
-echo mounting shares
-
-sudo mount -t vboxsf VB_Share ~/vb_share
+echo mount the shares with:
+echo "  sudo mount -t vboxsf VB_Share ~/vb_share"
 
 # fixes the mouse scroll issue
-imwheel 
-
-
+imwheel
